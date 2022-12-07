cb0c1bfe326ea6ac260d513be12e07dbefe17ea1
Keegan Bond <keeganbond123@gmail.com>
2022-12-06 22:00:22 -0500

    created tables, data, routes, controllers, and all required information for gathering my data

--- controllers/leagueArticleController.js
+++ controllers/leagueArticleController.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
+const { v4: uuidv4 } = require("uuid");
 
 exports.articles = async (req, res) => {
     try {
@@ -10,22 +11,23 @@ exports.articles = async (req, res) => {
     }
 };
 
-exports.addArticle = (req, res) => {
-    if (
-        !id ||
-        !article_title ||
-        !description ||
-        !created_at ||
-        !author ||
-        !image
-    ) {
+exports.addArticle = async (req, res) => {
+    console.log(req.body);
+    if (!req.body.article_title || !req.body.description || !req.body.image) {
         res.status(400).send(
             `Please make sure you have entered each field correctly`
         );
     }
+    const newArticle = {
+        id: uuidv4(),
+        article_title: req.body.article_title,
+        description: req.body.description,
+        author: req.body.author,
+        image: req.body.image,
+    };
     try {
-        const articleData = knex("league_articles").insert(req.body);
-        res.status(200).send(articleData);
+        const articleData = await knex("league_articles").insert(newArticle);
+        res.status(201).send(articleData);
     } catch (error) {
         res.status(400).send(`error creating article ${error}`);
     }
